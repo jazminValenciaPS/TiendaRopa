48db9e498500ae9b5e9648e56cb8b187af64f34d
jazmin valencia <jazminvalencia@MacBook-Air-de-jazmin.local>
2020-01-20 22:53:12 -0700

    subcategorias select

--- ropaTienda/resources/assets/js/consoleComponents/Subcategorias.vue
+++ ropaTienda/resources/assets/js/consoleComponents/Subcategorias.vue
@@ -1,33 +1,61 @@
+
+
 <template>
     <main class="main">
+        <div class="row" :class="{'mostrar' : modal}" >
+            <div class="col s6">
+                    <div class="input-field">
+                        <input  id="subcategoria" type="text" class="validate">
+                        <label for="subcategoria">Nombre</label>
 
-<div class="container">
-  <div class="input-field col s12">
-    <select id="selEstadosMateria">
-      <option value="" disabled selected>Seleccionar</option>
-      <option value="1">sólido</option>
-      <option value="2">líquido</option>
-      <option value="3">gaseoso</option>
-    </select>
-    <label>Estado de la Materia</label>
-  </div>
-  <a class="waves-effect waves-light btn btnRecorrer">Recorrer</a>
-</div>
-  </main>
-    </template>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+                        <select name="LeaveType" @change="onChange($event)" class="browser-default" v-model="key">
+                            <option value="" disabled selected>Selecciona la categoria</option>
+                            <option value="1">Ropa</option>
+                            <option value="2">Accesorios</option>
+                            <option value="3">Cosmeticos</option>
+                        </select>  
+                    </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="right col s2">
+                <a class="btn-floating btn-large waves-effect waves-light deep-orange lighten-4 right" @click="abrirModal('subcategoria','registrar')"><i class="brown-text material-icons">add</i></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12">    
+                <ul class="collection" v-for="subcategoria in arraySubcategoria" :key="subcategoria.id">
+
+                    <li class="collection-item avatar">
+                        <img src="img/topAzul.jpg" alt="" class="circle">
+                    </li>
+
+                     <p class="center" v-text="subcategoria.Nombre"><br></p>
+                    <a href="#!" class="secondary-content">
+                    <i class="material-icons" @click="abrirModal('categoria','actualizar',subcategoria)">create</i>
+                    <i class="material-icons">delete</i></a>
+                </ul>
+
+            </div>
+        </div>
+    </main>
+</template>
+
 <script>
-    $(document).ready(function() {
-        $('#selectValue').on('change',function(){
-        var data = $("#selectValue").find(":selected").val();
-
-        console.log(data);
-        });
-
-    });
-
+    export default {
+        data(){
+            return{
+                key: "",
+                nombre: '',
+                arraySubcategoria:[],
+                modal : 0,
+            }
+        },
+        methods: {
+            onChange(event) {
+                console.log(event.target.value)
+            }
+        }
+    }
 </script>
-
-    
